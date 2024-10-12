ad017271a6513e4fc1c9a1623218dba02aabe626
llllyyyynnnn <157394687+llllyyyynnnn@users.noreply.github.com>
2024-10-12 16:44:03 +0200

    wip functions, working on GetColumnSize

---
+++ Main.cs
@@ -0,0 +1,6 @@
+﻿using CSVFileDataManagement;
+
+CSVManager.Initialize();
+Console.WriteLine("Enter the name of the csv file you would like to read (*.csv)");
+CSVManager.SetActiveFile(Console.ReadLine());
+CSVManager.PrintData();
--- Program.cs
+++ Program.cs
@@ -1,1 +1,74 @@
-﻿
+﻿using System.IO;
+using System.Numerics;
+using System.Reflection;
+
+namespace CSVFileDataManagement
+{
+    internal class CSVManager
+    {
+        private static string activePath = string.Empty;
+        private static string activeFilePath = string.Empty;
+        private static string[] activeFileContents = Array.Empty<string>();
+        
+        private static char splitChar = ',';
+        private static char fillChar = '-';
+        private static char spaceChar = '|';
+
+
+        public static void Initialize()
+        {
+            activePath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            Console.WriteLine($"Active path has been set to {activePath}");
+        }
+
+        public static void SetActiveFile(string fileName)
+        {
+            activeFilePath = $"{activePath}/{fileName}";
+
+            if (File.Exists(activeFilePath))
+            {
+                Console.WriteLine($"{activeFilePath} was found, reading");
+                
+                string fileData = File.ReadAllText(fileName);
+                activeFileContents = fileData.Split(Environment.NewLine);
+            }
+            else
+            {
+                Console.WriteLine($"{activeFilePath} was not found, creating empty");
+                File.Create(activeFilePath);
+            }
+        }
+
+        private static int GetColumnSize(int index)
+        {
+            int largestStringLength = 0;
+
+            foreach(string str in activeFileContents)
+            {
+
+            }
+
+            return 0;
+        }
+
+        public static void PrintData()
+        {
+            Vector2 consoleSize = new Vector2(Console.WindowWidth, Console.WindowHeight);
+            string consoleFillerString = new string(fillChar, (int)consoleSize.X);
+
+            /*
+             for (int x = 0; x < activeFileContents.Count(); x++)
+            {
+                int linePosition = Console.GetCursorPosition().Top;
+                Console.SetCursorPosition(0, linePosition + 1);
+
+                string[] splitContents = activeFileContents[x].Split(splitChar);
+                for(int y = 0; y < splitContents.Count(); y++)
+                {
+
+                }
+            }
+             */
+        }
+    }
+}
